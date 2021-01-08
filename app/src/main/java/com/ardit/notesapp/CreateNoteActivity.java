d0fd3ad1399636a8bb9a4a7a57b88ffcce934b60
Konjuhi <ardit.konjuhi@hotmail.com>
2021-01-08 15:05:48 +0100

    ui

---
+++ app/src/main/java/com/ardit/notesapp/CreateNoteActivity.java
@@ -0,0 +1,26 @@
+package com.ardit.notesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class CreateNoteActivity extends AppCompatActivity {
+
+    ImageView imageBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_note);
+
+        imageBack = findViewById(R.id.imageBack);
+        imageBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+}
